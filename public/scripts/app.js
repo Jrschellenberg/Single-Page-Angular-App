cfc87cdd4fb2069b1f107efd7907576a0d644c69
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-06-06 10:34:31 -0500

    Started on ng model and service with categories

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,21 @@
 angular.module("app", ['ngRoute'])
-	.controller('RecipesController', function($scope){
-		
+	.controller('RecipesController', function($scope, dataService){
+		dataService.getCategories(function(response){
+			console.log(response.data);
+			$scope.categories = response.data;
+
+		});
 		
 		
 	})
 	.controller('RecipeDetailController', function($scope){
 		
+		
+	})
+	.service('dataService', function($http){
+		this.getCategories = function(callback){
+			$http.get('http://localhost:5000/api/categories')
+				.then(callback)
+		};
 		
 	});
